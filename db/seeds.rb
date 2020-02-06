59228b93fe3d7b8657815cd89836c7d68de0f90a
BearCub29 <poulichet.florian@gmail.com>
2020-02-06 20:16:09 +0100

    final version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'faker'
 city_id = []
-
 5.times do
   city = City.create!(name: Faker::Address.city)
   puts "Création d'une ville"
   city_id << city.id
 end
-
 dog_sitter_id = []
 20.times do
   dog_sitter = DogSitter.create!(name: Faker::Name.first_name, city_id: city_id.sample )
@@ -22,11 +20,10 @@ dog_sitter_id = []
 end
 dog_id = []
 20.times do
-  dog = Dog.create!(name: Faker::Name.first_name, city_id: city_id.sample)
+  dog = Dog.create!(name: Faker::DcComics.hero, city_id: city_id.sample)
   puts "Création d'un chien"
   dog_id << dog.id
 end
-
 5.times do
   stroll = Stroll.create!(dog_sitter_id: dog_sitter_id.sample,dog_id: dog_id.sample )
   puts "Création d'une promenade"
